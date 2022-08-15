f811ce40b813fdbd1ab76c0f68931b2c5a7e0af8
asad <asadtahir9999@gmail.com>
2022-08-14 20:42:20 -0500

    initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace stack_overload.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
